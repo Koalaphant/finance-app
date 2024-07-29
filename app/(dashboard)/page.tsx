337718b281f9c08dfcd86f6e8de7f8f11191acd4
Koalaphant <andrew.wardjones@icloud.com>
2024-07-29 12:28:50 +0100

    accounts GET api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { SignOutButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <SignOutButton redirectUrl="/"></SignOutButton>
-    </div>
-  );
+  return <div>Home Page</div>;
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,16 +1,27 @@
 import {z} from 'zod'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { zValidator } from '@hono/zod-validator'
+// import { zValidator } from '@hono/zod-validator'
+
+import accounts from './accounts'
+import { HTTPException } from 'hono/http-exception'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-    console.log(c)
-    return c.json({hello: 'world'})
+app.onError((err,c) => {
+    if (err instanceof HTTPException){
+        return err.getResponse()
+    }
+
+    return c.json({ error: 'Internal error' }, 500)
 })
+
+const routes = app
+  .route('/accounts', accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes;
